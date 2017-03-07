becd2140288bfed17cf3b448ba8577ee6dc5cfee
colinforzeal <colinforzeal@yandex.ru>
2017-03-07 23:54:07 +0300

    now frontcontroller is able to map all urls

--- src/main/java/FrontController.java
+++ src/main/java/FrontController.java
@@ -10,7 +10,13 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 
-//todo map all routes
+@WebServlet("/app/*")
 public class FrontController extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ContactListCommand command = new ContactListCommand(req, resp);
+        command.process();
+        command.forward("index");
 
+    }
 }
---
+++ src/main/java/FrontFilter.java
@@ -0,0 +1,29 @@
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@WebFilter("/*")
+public class FrontFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        String uri = request.getRequestURI();
+
+        if (uri.startsWith("/WEB-INF")) {
+            filterChain.doFilter(servletRequest, servletResponse);
+        } else {
+            servletRequest.getRequestDispatcher("/app" + uri)
+                    .forward(servletRequest, servletResponse);
+        }
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
