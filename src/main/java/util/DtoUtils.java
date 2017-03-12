c59a924e08945da4fc88bbb5b8dcd01cb4743cfd
colinforzeal <colinforzeal@yandex.ru>
2017-03-12 14:47:59 +0300

    configured liquibase & tested jdbc statement

--- src/main/java/util/DtoUtils.java
+++ src/main/java/util/DtoUtils.java
@@ -22,11 +22,12 @@ public class DtoUtils {
     }
 
     private static String createAddress(Address address){
-        return nullToEmpty(address.getCountry())
-                + nullToEmpty(address.getCity())
-                + nullToEmpty(address.getCountry())
-                + nullToEmpty(address.getAddress())
-                + address.getZip();
+//        return nullToEmpty(address.getCountry())
+//                + nullToEmpty(address.getCity())
+//                + nullToEmpty(address.getCountry())
+//                + nullToEmpty(address.getAddress())
+//                + address.getZip();
+        return "address";
     }
 
     private static String nullToEmpty(String str) {
