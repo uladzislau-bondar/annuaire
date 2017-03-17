e1d895bee3a37af8300e66b49e4eff001ef8411f
colinforzeal <colinforzeal@yandex.ru>
2017-03-17 16:42:08 +0300

    added addressdao skeleton

---
+++ src/main/java/dao/AddressDao.java
@@ -0,0 +1,33 @@
+package dao;
+
+
+import entities.Address;
+
+import java.util.List;
+
+public class AddressDao extends AbstractTemplateDao<Address, Long {
+    @Override
+    public void save(Address address) {
+
+    }
+
+    @Override
+    public List<Address> getAll() {
+        return null;
+    }
+
+    @Override
+    public Address getById(Long id) {
+        return null;
+    }
+
+    @Override
+    public void update(Address address) {
+
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+}
