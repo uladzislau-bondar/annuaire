ffb99025678525b57530d698dc17d62902acd9f5
colinforzeal <colinforzeal@yandex.ru>
2017-03-19 13:27:54 +0300

    added phonedao

---
+++ src/main/java/dao/PhoneDao.java
@@ -0,0 +1,33 @@
+package dao;
+
+
+import entities.Phone;
+
+import java.util.List;
+
+public class PhoneDao extends AbstractTemplateDao<Phone, Long {
+    @Override
+    public Long save(Phone entity) {
+        return null;
+    }
+
+    @Override
+    public List<Phone> getAll() {
+        return null;
+    }
+
+    @Override
+    public Phone getById(Long id) {
+        return null;
+    }
+
+    @Override
+    public void update(Phone entity) {
+
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+}
