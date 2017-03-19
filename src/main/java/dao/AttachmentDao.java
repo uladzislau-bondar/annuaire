c444464d31432409c7399b7816468ce5db93d434
colinforzeal <colinforzeal@yandex.ru>
2017-03-19 20:11:31 +0300

    added boilerplate for attachmentdao

---
+++ src/main/java/dao/AttachmentDao.java
@@ -0,0 +1,33 @@
+package dao;
+
+
+import entities.Attachment;
+
+import java.util.List;
+
+public class AttachmentDao extends AbstractTemplateDao<Attachment, Long> {
+    @Override
+    public Long save(Attachment attachment) {
+        return null;
+    }
+
+    @Override
+    public List<Attachment> getAll() {
+        return null;
+    }
+
+    @Override
+    public Attachment getById(Long id) {
+        return null;
+    }
+
+    @Override
+    public void update(Attachment attachment) {
+
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+}
