133b0fe270d1f8136d922f7f9fd96ee2e5e0fc69
colinforzeal <colinforzeal@yandex.ru>
2017-03-19 13:31:30 +0300

    added phone sql constants

---
+++ src/main/java/db/constants/PhoneContstants.java
@@ -0,0 +1,13 @@
+package db.constants;
+
+
+public class PhoneContstants {
+    String SAVE = "INSERT INTO phones (contactId, countryCode, number, type, comment)" +
+            " VALUES (?, ?, ?, ?, ?)";
+    String GET_ALL = "SELECT * FROM phones";
+    String GET_BY_ID = "SELECT * FROM phones WHERE id = ?";
+    String GET_BY_CONTACT_ID = "SELECT * FROM phones WHERE contactId = ?";
+    String UPDATE = "UPDATE phones SET contactId = ?, countryCode = ?, number = ?," +
+            "type = ?, comment = ? WHERE id = ?";
+    String DELETE = "DELETE FROM phones WHERE id = ?";
+}
