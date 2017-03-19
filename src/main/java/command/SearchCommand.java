c9b91a904bb4e9b0cb58c07dec7cd3853151d6f6
colinforzeal <colinforzeal@yandex.ru>
2017-03-19 21:13:41 +0300

    added boilerplate to search command POST processing

--- src/main/java/command/SearchCommand.java
+++ src/main/java/command/SearchCommand.java
@@ -2,11 +2,13 @@ package command;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import util.StringUtils;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Date;
 
 
 public class SearchCommand extends AbstractCommand {
@@ -49,5 +51,14 @@ public class SearchCommand extends AbstractCommand {
     }
 
     private void search() {
+        String firstName = request.getParameter("firstName");
+        String lastName = request.getParameter("lastName");
+        String middleName = request.getParameter("middleName");
+        Date dateOfBirth = StringUtils.emptyToDate(request.getParameter("dateOfBirth"));
+        String sex = request.getParameter("sex");
+        String citizenship = request.getParameter("citizenship");
+        String maritalStatus = request.getParameter("maritalStatus");
+
+        //todo process retrieving contacts from db and redirecting to index page
     }
 }
