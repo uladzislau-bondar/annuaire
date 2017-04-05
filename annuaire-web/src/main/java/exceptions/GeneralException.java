911cafd3d8506fea74f4482c4e5cf235617c9efe
colinforzeal <colinforzeal@yandex.ru>
2017-04-05 13:41:43 +0300

    added general exception

---
+++ annuaire-web/src/main/java/exceptions/GeneralException.java
@@ -0,0 +1,20 @@
+package exceptions;
+
+
+public class GeneralException extends Exception{
+    public GeneralException() {
+        super();
+    }
+
+    public GeneralException(String message) {
+        super(message);
+    }
+
+    public GeneralException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public GeneralException(Throwable cause) {
+        super(cause);
+    }
+}
