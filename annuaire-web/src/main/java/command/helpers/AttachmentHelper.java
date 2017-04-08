0ab138f2025230be9eaa05afc9300c586028ffe7
colinforzeal <colinforzeal@yandex.ru>
2017-04-08 16:17:11 +0300

    added attachment utf-8

--- annuaire-web/src/main/java/command/helpers/AttachmentHelper.java
+++ annuaire-web/src/main/java/command/helpers/AttachmentHelper.java
@@ -4,22 +4,25 @@ package command.helpers;
 import com.annuaire.dto.AttachmentDatabaseDto;
 
 import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 public class AttachmentHelper extends AbstractHelper {
     public AttachmentHelper(HttpServletRequest request, HttpServletResponse response) {
         super(request, response);
     }
 
-    public void renderAttachment(AttachmentDatabaseDto attachment) throws IOException {
+    public void renderAttachment(AttachmentDatabaseDto attachment) throws ServletException, IOException {
         addDownloadParams(attachment.getName(), attachment.getFile());
         renderFile(attachment.getFile());
     }
 
-    private void addDownloadParams(String fileName, File attachment) {
+    private void addDownloadParams(String fileName, File attachment) throws ServletException{
         ServletContext context = request.getServletContext();
         String mimeType = context.getMimeType(attachment.getAbsolutePath());
         if (mimeType == null) {
@@ -28,9 +31,17 @@ public class AttachmentHelper extends AbstractHelper {
 
         response.setContentType(mimeType);
         response.setContentLength((int) attachment.length());
+        response.setCharacterEncoding("utf-8");
 
         String headerKey = "Content-Disposition";
-        String headerValue = String.format("attachment; filename=\"%s\"", fileName);
+
+        String headerValue = null;
+        try {
+            fileName = URLEncoder.encode(fileName,"UTF-8");
+            headerValue = String.format("attachment; filename=\"%s\"", fileName);
+        } catch (UnsupportedEncodingException e) {
+            throw new ServletException(e);
+        }
         response.setHeader(headerKey, headerValue);
     }
 }
